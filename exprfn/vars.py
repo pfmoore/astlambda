05ffb2c2cbe6b9d03c9b9f0adfdfc565bb574e91
Paul Moore <p.f.moore@gmail.com>
2021-02-26 09:51:06 +0000

    Add some more vars, _ and _0 to _9

--- exprfn/vars.py
+++ exprfn/vars.py
@@ -1,4 +1,8 @@
 from . import ExprFn
 
-for name in "abcdefghijklmnopqrstuvwxyz":
+for name in "abcdefghijklmnopqrstuvwxyz_":
+    globals()[name] = ExprFn.name(name)
+
+for n in range(10):
+    name = f"_{n}"
     globals()[name] = ExprFn.name(name)
